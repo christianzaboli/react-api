87153045fa71ff792832dd12cecadc2c62f8b27d
christianzaboli <zabolichristian@gmail.com>
2025-10-02 22:45:54 +0200

    milestone 1, stampo di data delle due liste in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import MyHeader from './assets/MyHeader'
 import MyMain from './assets/MyMain'
 import MyFooter from './assets/MyFooter'
+
 function App() {
 
   return (
--- src/assets/MyMain.jsx
+++ src/assets/MyMain.jsx
@@ -1,4 +1,22 @@
+import { useState, useEffect } from "react";
+
 function MyMain() {
+    const [attori, setAttori] = useState([])
+    const [attrici, setAttrici] = useState([])
+
+    function fetchAttori() {
+        axios.get('https://lanciweb.github.io/demo/api/actors/')
+            .then((res) => console.log(res.data))
+    }
+    function fetchAttrici() {
+        axios.get('https://lanciweb.github.io/demo/api/actresses/')
+            .then((res) => console.log(res.data))
+    }
+
+    useEffect(fetchAttori, []);
+    useEffect(fetchAttrici, []);
+    // console.log(attori, attrici);
+
     return (
         <main>
 
